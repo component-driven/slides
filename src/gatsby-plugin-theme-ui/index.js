a5ff13c766d42c17fa4b3701ff9a9c26bea33d24
Andrey Okonetchnikov <andrey@okonet.ru>
2021-04-26 16:14:06 +0200

    Use MonoLisa Balanced variation from the preview release

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,11 +2,16 @@ import theme from '@component-driven/mdx-deck-theme';
 
 export default {
     ...theme,
+    googleFont: "https://www.monolisa.dev/api/fonts/preview",
     styles: {
         ...theme.styles,
         root: {
             ...theme.styles.root,
             fontSize: '16px', // Change the base font size for the website
+            fontFeatureSettings: '"ss02" on',
+            "code": {
+                fontFeatureSettings: '"ss02" off',
+            }
         },
     },
 };
